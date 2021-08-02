c0918b6b9c8dc09bae235d3a896abae23b206c90
15919873635 <15919873635@126.com>
2021-08-02 21:42:25 +0800

    project init

--- packages/bin/mock.ts
+++ packages/bin/mock.ts
@@ -5,5 +5,4 @@ import { program } from "commander";
 program
     .version(require("../../package.json").version)
     .command("codegen", "codegen files")
-    .command("server", "mock server operation")
     .parse(process.argv);
