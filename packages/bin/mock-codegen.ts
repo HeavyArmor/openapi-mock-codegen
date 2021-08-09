1d09eee6c8821378676ed398da02c571a1cc8b2d
15919873635 <15919873635@126.com>
2021-08-09 23:00:24 +0800

    add --api parameter to codegen commander

--- packages/bin/mock-codegen.ts
+++ packages/bin/mock-codegen.ts
@@ -9,15 +9,24 @@ program
     .name("mock-codegen")
     .option("-td, --templateDir <dir>", "template files dir", path.resolve(process.cwd(), "templates"))
     .option("-cd, --codeDir <dir>", "the path code generate to", path.resolve(process.cwd(), "server"))
+    .option("--api <dir>", "the path of OpenAPI definition file")
     .action((dir, cmd) => {
         const templateDir = dir.templateDir;
         const codeDir = dir.codeDir;
-        if(path.isAbsolute(templateDir)) {
-            if(!fs.existsSync(templateDir)) {
-                console.error(`The value of dir ${templateDir} is not exists`);
-                process.exit(1);
-            }
-            generate(templateDir, path.resolve(codeDir, "routes"));
+        const apiDir = dir.api;
+        let computedTemplateDir = apiDir ? 
+            (path.isAbsolute(apiDir) ? apiDir : path.resolve(process.cwd(), apiDir)) : 
+            (path.isAbsolute(templateDir) ? templateDir : path.resolve(process.cwd(), templateDir));
+        let computedCodeDir = path.isAbsolute(codeDir) ? 
+            path.resolve(codeDir, "routes") : 
+            path.resolve(process.cwd(), codeDir, "routes");
+        if(!fs.existsSync(computedTemplateDir)) {
+            console.error(`The value of dir ${computedTemplateDir} is not exists`);
+            process.exit(1);
+        } else if(apiDir && !fs.statSync(computedTemplateDir).isFile()) {
+            console.error(`The value ${computedTemplateDir} is not a file`);
+            process.exit(1);
         }
+        generate(computedTemplateDir, computedCodeDir);
     })
     .parse(process.argv);
